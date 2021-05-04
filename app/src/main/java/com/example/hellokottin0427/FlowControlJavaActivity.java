53a57b39b1cc43325f2cb9072974b59df1a07573
murod1 <jmurod2000@gmail.com>
2021-05-04 10:13:55 +0900

    FlowControl

--- app/src/main/java/com/example/hellokottin0427/FlowControlJavaActivity.java
+++ app/src/main/java/com/example/hellokottin0427/FlowControlJavaActivity.java
@@ -38,9 +38,7 @@ public class FlowControlJavaActivity extends AppCompatActivity {
                 switch(number){
                     case 4:
                     case 9:
-                        if(number==4){
-                            btnLD.setText("실행 for 4 or  9");
-                        }
+                            btnLD.setText("실행 "+number);
                         break;
                      default:
                          btnLD.setText("실행");
